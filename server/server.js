29425016b5ae010567becfefe69df6b5702b870e
Jakub Bialobrzeski <kubikowopl@gmail.com>
2026-03-13 12:08:47 +0100

    Ex 1 - Added the following
    Backend:
    getMovie(movieName): fetches the api and parses the input into right data format
    
    adapted GET /movies endpoint: it grabs the movies and sends theses as a response
    
    Frontend:
    Created factory methods for automatically scaling the website with each added movie and styling as described on the README

--- server/server.js
+++ server/server.js
@@ -5,11 +5,39 @@ const app = express()
 // Serve static content in directory 'files'
 app.use(express.static(path.join(__dirname, 'files')));
 
-// Configure a 'get' endpoint for data..
-app.get('/movies', function (req, res) {
-  // Part 1: Remove the next line and replace with your code
-  res.send('!dlrow olleH')
+app.get('/movies', async function (req, res) {
+ let movies = [];
+  
+  movies.push(await getMovie('Avatar'));
+  movies.push(await getMovie('Cars'));
+  movies.push(await getMovie('Avengers'));
+
+  res.send(movies);
 })
+
+async function getMovie(movieName){
+  // I know I shouldn't be pushing apikeys but idc about this one
+  url = "https://www.omdbapi.com/?t=" + movieName + "&apikey=70592b79";
+  const response = await fetch(url);
+  const data = await response.json();
+
+  const {Title,Plot,Poster} = data;
+  
+  // 2011-10-05T14:48:00.000Z -> split at T and take the first part to get just the date
+  const Released = new Date(data.Released).toISOString().split('T')[0];
+
+  // Runtime is in the format "162 min", so split at the space and take the first part to get just the number
+  const Runtime = Number(data.Runtime.split(' ')[0]);
+  Genres = data.Genre.split(',');
+  Directors = data.Director.split(',');
+  Writers = data.Writer.split(',');
+  Actors = data.Actors.split(',');
+  Metascore = Number(data.Metascore);
+  imdbRating = Number(data.imdbRating)
+
+  const movie = {Title,Released,Runtime,Genres,Directors,Writers,Actors,Plot,Poster,Metascore,imdbRating};
+  return movie;
+}
 
 app.listen(3000)
 
